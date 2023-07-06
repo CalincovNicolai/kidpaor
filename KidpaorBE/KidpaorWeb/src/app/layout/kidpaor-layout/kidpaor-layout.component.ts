19fefb889dff73f7ac031ec018c40dfe2056cfa6
CalincovNicolai <77538373+CalincovNicolai@users.noreply.github.com>
2023-07-06 22:21:28 +0300

    Adjust layout

--- KidpaorBE/KidpaorWeb/src/app/layout/kidpaor-layout/kidpaor-layout.component.ts
+++ KidpaorBE/KidpaorWeb/src/app/layout/kidpaor-layout/kidpaor-layout.component.ts
@@ -1,10 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-kidpaor-layout',
   templateUrl: './kidpaor-layout.component.html',
   styleUrls: ['./kidpaor-layout.component.scss']
 })
-export class KidpaorLayoutComponent {
+export class KidpaorLayoutComponent implements OnInit {
   @Input() image: string = '';
+  showComponent: boolean = false;
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  ngOnInit(): void {
+    this.route.url.subscribe(url => {
+      const currentRoute = url[0].path;
+      this.showComponent = (currentRoute !== 'support') && (currentRoute !== 'about');
+    });
+  }
 }
--- KidpaorBE/KidpaorWeb/src/app/modules/about-us-feature/about-us-feature.module.ts
+++ KidpaorBE/KidpaorWeb/src/app/modules/about-us-feature/about-us-feature.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AboutUsPageComponent } from './about-us-page/about-us-page.component';
+import { LayoutModule } from '../../layout/layout.module';
 
 @NgModule({
   declarations: [
     AboutUsPageComponent
   ],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        LayoutModule
+    ]
 })
 export class AboutUsFeatureModule { }
